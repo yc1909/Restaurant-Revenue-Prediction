ad1e884f976839bc312dd7078f3b26240ea21b6d
Yashu Chauhan <chauhanyashu1909@gmail.com>
2024-06-09 00:22:44 +0530

    Logger and Exception Handling

---
+++ src/exception.py
@@ -0,0 +1,30 @@
+import sys
+from src.logger import logging
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+
+    file_name= exc_tb.tb_frame.f_code.co_filename
+    line_num = exc_tb.tb_lineno
+    error_msg = str(error)
+
+    error_message = f'The error is python script name {file_name}, in line number {line_num} and error message as {error_msg}'
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail = error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Division by Zero")
+        raise CustomException(e,sys)
